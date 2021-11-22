bd0b85508d18e9a434a059dd4a46cd15ac416929
h_dinari <h_dinari@modernisc.com>
2021-11-22 09:29:01 +0330

    #initialCommit

---
+++ src/main/java/test/program/Main.java
@@ -0,0 +1,10 @@
+package test.program;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is first program on Github");
+        System.out.println("Commit to the master");
+        System.out.println("commit2 to master");
+        System.out.println("commit3 to master");
+    }
+}
